b4197e9b95ce718b12c5deb85e89cd4afe266979
Skyler Grey <skyler@clicksminuteper.net>
2022-05-22 21:00:40 +0100

    Let flake run under all systems

--- flake.nix
+++ flake.nix
@@ -4,26 +4,30 @@
   outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
  
-    pkgs = import nixpkgs {
-      inherit system;
-    };
+    forAllSystems = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.supported.hydra (system: f system);
   in rec {
-    packages."${system}".pridefetch = pkgs.stdenv.mkDerivation {
-      name = "pridefetch";
-      buildInputs = [
-        (pkgs.python39.withPackages (pythonPackages: with pythonPackages; [
-          distro          
-        ]))
-      ];
-      unpackPhase = "true";
-      installPhase = ''
-        mkdir -p $out/bin
-        cp ${./pridefetch} $out/bin/pridefetch
-        cp ${./packages.py} $out/bin/packages.py
-        chmod +x $out/bin/pridefetch
-      '';
-    };
+    packages = forAllSystems (system: let 
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      pridefetch = pkgs.stdenv.mkDerivation {
+        name = "pridefetch";
+        buildInputs = [
+          (pkgs.python39.withPackages (pythonPackages: with pythonPackages; [
+            distro          
+          ]))
+        ];
+        unpackPhase = "true";
+        installPhase = ''
+          mkdir -p $out/bin
+          cp ${./pridefetch} $out/bin/pridefetch
+          cp ${./packages.py} $out/bin/packages.py
+          chmod +x $out/bin/pridefetch
+        '';
+      };
+    });
 
-    defaultPackage."${system}" = packages."${system}".pridefetch;
+    defaultPackage = forAllSystems (system: self.packages.${system}.pridefetch);
   };
 }
