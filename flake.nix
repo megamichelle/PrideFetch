02cb0c50c7870eb983c06862d3433b1df815e0ae
Skyler <skyler3665@gmail.com>
2022-06-05 13:37:33 +0100

    Update the flake such that it uses nixpkgs' package (#17)
    * Update the flake such that it uses nixpkgs' package with an overriden source rather than keeping a copy
    
    * Update flake.nix
    
    * Update the flake lock file, and make supported systems match the package
    
    * remove surplus whitespace

--- flake.nix
+++ flake.nix
@@ -1,55 +1,26 @@
 {
   description = "A flake to run pridefetch";
 
-  outputs = { self, nixpkgs }: let
+  inputs = {
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable }: let
     system = "x86_64-linux";
- 
-    forAllSystems = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.supported.hydra (system: f system);
+
+    forAllSystems = f: nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all (system: f system);
   in rec {
-    packages = forAllSystems (system: let 
+    packages = forAllSystems (system: let
       pkgs = import nixpkgs {
         inherit system;
       };
-    in {
-      pridefetch = pkgs.stdenv.mkDerivation {
-        name = "pridefetch";
-        buildInputs = with pkgs; [
-          (python39.withPackages (pythonPackages: with pythonPackages; [
-            distro          
-          ]))
-          zip
-        ];
-        unpackPhase = "true";
-        installPhase = ''
-          mkdir -p $out/bin
-          cd ${./src}
-          zip -r $out/pridefetch.zip *
-          echo '#!/usr/bin/env python' | cat - $out/pridefetch.zip > $out/bin/pridefetch
-          chmod +x $out/bin/pridefetch
-          rm $out/pridefetch.zip
-        '';
-        meta = with pkgs.lib; {
-          description = "Print out system statistics with pride flags";
-          longDescription = ''
-            Pridefetch prints your system statistics (similarly to neofetch, screenfetch or pfetch) along with a pride flag.
-            The flag which is printed is configurable, as well as the width of the output.
-          '';
-          homepage = https://github.com/SpyHoodle/pridefetch;
-          license = "bsd"; 
-          # https://static.domenkozar.com/nixpkgs-manual-sphinx-exp/meta.xml.html
-          # > Catch-all for licenses that are essentially similar to the original BSD license with the advertising clause removed, i.e. permissive non-copyleft free software licenses. This includes the X11 (“MIT”) License.
-          # our license is MIT, so BSD is set here
-          maintainers = [
-            {
-              email = "skyler3665@gmail.com";
-              github = "minion3665";
-              githubId = 34243578;
-              name = "Skyler Grey";
-            }
-          ];  # TODO: Replace this with a reference to the maintainer list after adding myself to it
-          platforms = systems.supported.hydra;
-        };
+      pkgs-unstable = import nixpkgs-unstable {
+        inherit system;
       };
+    in {
+      pridefetch = pkgs-unstable.pridefetch.overrideAttrs (finalAttrs: previousAttrs: {
+        src = builtins.path { path = ./.; name = "pridefetch"; };
+      });
     });
 
     defaultPackage = forAllSystems (system: self.packages.${system}.pridefetch);
